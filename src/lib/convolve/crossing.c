645044befd4051d59d33133054c5b78ed09f59b6
eran <>
2008-10-14 10:49:47 +0000

    Updated.

--- src/lib/convolve/crossing.c
+++ src/lib/convolve/crossing.c
@@ -49,8 +49,6 @@ static char *Id = "$Id$, Blab, UiO";
 
 
 
-#ifndef MAIN
-
 /*F:crossing*
 
 ________________________________________________________________
@@ -154,84 +152,3 @@ BiffStatus crossing(IBAND iband, IBAND oband, int t)
 
   return(0);
 }
-
-#endif /* not MAIN */
-
-/*P:crossing*
-
-________________________________________________________________
-
-		crossing
-________________________________________________________________
-
-Name:		crossing - find zero crossing or any other crossing
-
-Syntax:		crossing [-t <t>] <inimage> <outimage>
-
-Description:	Can be used to find zero crossing of second derivative,
-		laplace. Can also be used to determine any other kind
-		of crossing. Pixels below or equal to 't' are set if the pixel
-		to the right or below is above 't', pixels above 't' are
-		set if the pixel to the right or below is below or equal to
-		't'. Pixels that are "set" are set to the maximum absolute
-		difference of the two neighbours, to indicate the strength
-		of the edge.
-
-		| IF (crossing t)
-		|   out(x,y) = MAX(ABS(in(x,y)-in(x+1,y)), 
-		|                  ABS(in(x,y)-in(x,y+1)))
-		| ELSE
-		|   out(x,y) = 0
-
-Restrictions:   'inimage' must have bands with pixel type unsigned byte.
-
-Options:        &-t t
-                Crossing value. Default is 127.
-
-See also:	crossing(3), contour(1)
-
-Author:		Tor Lønnestad, BLAB, Ifi, UiO
-
-Examples:	crossing -t 100 mona.img monacross.img
-
-Id: 		$Id$
-________________________________________________________________
-
-*/
-
-#ifdef MAIN
-
-int main(int argc, char **argv)
-{
-  IMAGE img;
-  int t, nbands, bn;
-  char *args;
-
-  Iset_message(TRUE);
-  Iset_abort(TRUE);
-  InitMessage(&argc, argv, xite_app_std_usage_text(
-    "Usage: %s [<options>] <inimage> <outimage>\n"));
-
-  if (argc == 1) Usage(1, NULL);
-  args = argvOptions(argc, argv);
-
-  t = read_iswitch(&argc, argv, "-t", 127);
-
-  if (argc != 3) Usage(1, "Illegal number of arguments.\n");
-
-  img = Iread_image(argv[1]);
-  if (!img) Error(2, "Error in reading image.\n");
-  nbands = Inbands(img);
-
-  for (bn=1; bn LE nbands; bn++) {
-    if (crossing(img[bn], img[bn], t))
-      Error(2, "Error in crossing() while processing band %d.\n", bn);
-  }
-
-  Ihistory(img, argv[0], args);
-  Iwrite_image(img, argv[2]);
-
-  return(0);
-}
-
-#endif /* MAIN */
--- src/lib/convolve/laplace.c
+++ src/lib/convolve/laplace.c
@@ -49,8 +49,6 @@ static char *Id = "$Id$, Blab, UiO";
 #endif
 #include <xite/message.h>
 
-#ifndef MAIN
-
 /*F:laplace*
 
 ________________________________________________________________
@@ -100,98 +98,3 @@ void laplace(IBAND input, IBAND output, int a, int b, int c, double scale, doubl
   conv[4] = c;
   conv3x3(input, output, conv, scale, offset, verbose);
 }
-
-#endif /* not MAIN */
-
-
-
-
-/*P:laplace*
-
-________________________________________________________________
-
-		laplace
-________________________________________________________________
-
-Name:		laplace - edge and line detection
-Syntax:		| laplace [-scale <n>] [-offset <m>]  [-title <title>] 
-                |          [-verbose] <inimage> <outimage> a b c 
-Description:    'laplace' perform a 3x3 convolution on the input image
-                'inimage' and write the result to 'outimage'.
-                The convolution kernel is specified by a, b and c
-		|
-		|     a b a
-		|     b c b
-		|     a b a
-                |
-                
-                If '-scale' (-s) and '-offset' (-o) is specified all 
-		data are scaled:
-                | pix = (conv. result) * scale + offset;
-                Othewise an autoscaling is performed.
-
-		'-title' (-t) 'title' is used to set a new title on the 
-		output image.
-
-                If '-verbose' (-v) is specified 'laplace' writes a message
-                if the convolution sum is less than 0 or greater than 255.
-                If autoscale is performed the scale and offset factors are
-                displayed
-Example:	| laplace mona.img m.img -scale 1.5 -1 -1 8
-See also:       laplace(3), lapZeroCross(1), conv3x3(1)
-Author:		Otto Milvang
-Id: 		$Id$
-________________________________________________________________
-
-*/
-
-
-
-#ifdef MAIN
-
-int main(int argc, char **argv)
-{
-  IMAGE img, img2;
-  int i;
-  char *title, txt[132];
-  double scale, offset;
-  int log, a, b, c;
-
-  Iset_message(TRUE);
-  Iset_abort(TRUE);
-  InitMessage(&argc, argv, xite_app_std_usage_text(
-    "Usage: %s [-s <scale>] [-o <offset>] [-t <title>] [-v]\n\
-       <inimage> <outimage> <a> <b> <c> \n"));
-
-  if (argc == 1) Usage(1, NULL);
-  title        = read_switch(&argc, argv, "-title", 1, NULL);
-  scale        = read_dswitch(&argc, argv, "-scale", 0.0);
-  offset       = read_dswitch(&argc, argv, "-offset", 0.0);
-  log          = read_bswitch(&argc, argv, "-verbose");
-  title        = read_switch(&argc, argv, "-t", 1, title);
-  scale        = read_dswitch(&argc, argv, "-s", scale);
-  offset       = read_dswitch(&argc, argv, "-o", offset);
-  log          = log || read_bswitch(&argc, argv, "-v");
-
-  if (argc != 6) Usage(1, "Illegal number of arguments.\n");
-
-  a = atoi(argv[3]);
-  b = atoi(argv[4]);
-  c = atoi(argv[5]);
-
-  img = Iread_image(argv[1]);
-  if (img == NULL) {
-    Error(2, "Can't read input file %s\n", argv[1]);
-
-  }
-  img2 = Icopy_init(img);
-  for (i=1; i <= Inbands(img); i++)
-    laplace(img[i], img2[i], a, b, c, scale, offset, log);
-  sprintf(txt,"%d %d %d", a,b,c);
-  Ihistory(img2,argv[0],txt);
-  if (title) Iset_title(img2, title);
-  Iwrite_image(img2,argv[2]);
-  return(0);
-}
-
-#endif /* MAIN */
