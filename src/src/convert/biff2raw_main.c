126275881d1c6fdcc114a34ac480b58f2a596550
eran <>
2008-10-14 10:11:28 +0000

    Renamed biff2raw.c to biff2raw_main.c.

--- src/src/convert/biff2raw_main.c
+++ src/src/convert/biff2raw_main.c
@@ -35,11 +35,24 @@ static char *Id = "$Id$, Blab, UiO";
 #include <xite/includes.h>
 #include <xite/biff.h>
 #include <xite/convert.h>
-#include XITE_STDIO_H
+#ifdef HAVE_STDIO_H
+#  include <stdio.h>
+#endif
 #include <stdlib.h>
 #include XITE_UNISTD_H
-#include XITE_STRING_H
-#include XITE_TYPES_H
+#ifdef HAVE_UNISTD_H
+#  include <unistd.h>
+#endif
+#ifdef HAVE_STRINGS_H
+#  include <strings.h>
+#else
+#  ifdef HAVE_STRING_H
+#    include <string.h>
+#  endif
+#endif
+#ifdef HAVE_SYS_TYPES_H
+#  include <sys/types.h>
+#endif
 #include <xite/blab.h>
 #include <xite/utils.h>
 
@@ -51,98 +64,6 @@ static char *Id = "$Id$, Blab, UiO";
 #endif
 
 
-
-#ifndef MAIN
-
-/*F:biff2raw*
-
-________________________________________________________________
-
-		biff2raw
-________________________________________________________________
-
-Name:		biff2raw - write a biff band to a raw data file
-
-Syntax:         | #include <xite/convert.h>
-		|
-                | int biff2raw( IBAND band, char* filename,
-                |    char* type, int pgm );
-
-Description:    'biff2raw' dumps the contents of 'band' as raw, binary
-		data to the file 'filename'. Give filename = "", '-' or NULL
-		for writing to stdout. The band is written in samples-lines
-		order. 'type' may be one of:
-
-		&w
-		Truncate or create for writing.
-
-		&a
-		Append; open for writing at end of file, or create for writing.
-
-		-&
-		If 'pgm' is true, a pgm header is written before the data.
-
-		Accepts tilde in file name. Accepts all pixel types.
-
-Restrictions:   Writing ('biff2raw') and reading ('raw2biff') on hosts
-		with different byte order will only work for byte bands.
-
-Return value:   | 0 - ok
-		| 1 - bad 'type' argument, should be "w" or "a"
-		| 2 - could not open file
-		| 3 - could not write the whole band
-
-Author:		Tor Lønnestad, BLAB, Ifi, UiO
-
-Id:		$Id$ $Revision$
-________________________________________________________________
-
-*/
-
-#ifndef FUNCPROTO
-int biff2raw(band, filename, type, pgm)
-IBAND band;
-char *filename, *type;
-int pgm;
-#else /* FUNCPROTO */
-int biff2raw(IBAND band, char *filename, char *type, int pgm)
-#endif /* FUNCPROTO */
-{
-  FILE *f;
-  int y, xsize, ysize, pt, bytesPrPix, fd, linelen;
-  char *ptr;
-  char buf[100];
-
-  pt = Ipixtyp(band);
-  if (strcmp(type, "w") AND strcmp(type, "a"))
-    return(Error(1,"biff2raw: Bad 'type' argument\n")); 
-  if (!filename || strlen(filename) == 0 || !strcmp(filename, "-")) f = stdout;
-  else f = fopen(tilde_expand(filename), type);
-  if (!f) return(Error(2, "biff2raw: Could not open file %s\n", filename));
-  fd = fileno(f);
-
-  xsize      = Ixsize(band);
-  ysize      = Iysize(band);
-  bytesPrPix = Ipixsize(pt)/8;
-  linelen    = xsize * bytesPrPix;
-  if (pgm && Ipixtyp(band) != Iu_byte_typ) 
-      Error(2, "%s\n",
-	   "pgm is only implemented for bands with pixel type unsigned byte.");
-  if (pgm) {
-    sprintf(buf, "P5\n%d %d\n%d\n", xsize, ysize, 255);
-    write(fd, buf, strlen(buf));
-  }
-  for (y=1; y LE ysize; y++) {
-    ptr = (char*)((size_t)(&(band[y][0])) + bytesPrPix);
-    if (write(fd, ptr, linelen) NE linelen)
-      return(Error(3, "biff2raw: Could not write the whole band\n"));
-  }
-  if (f != stdout) fclose(f);
-
-  return(0);
-}
-
-#endif /* not MAIN */
 
 /*P:biff2raw*
 
@@ -211,13 +132,7 @@ ________________________________________________________________
 
 #ifdef MAIN
 
-#ifndef FUNCPROTO
-int main(argc,argv)
-int argc;
-char *argv[];
-#else /* FUNCPROTO */
 int main(int argc, char **argv)
-#endif /* FUNCPROTO */
 {
   IMAGE i;
   IBAND b;
@@ -279,5 +194,3 @@ int main(int argc, char **argv)
   }
   return(0);
 }
-
-#endif /* MAIN */
