2bc93a668a9063434d5898e1201df73a02b2ebda
eran <>
2008-10-14 09:03:14 +0000

    Updated

--- src/lib/combine/combine.c
+++ src/lib/combine/combine.c
@@ -38,9 +38,13 @@ static char *Id = "$Id$, Blab, UiO";
 #include <xite/biff.h>
 #include <xite/combine.h>
 #include <xite/message.h>
-#include XITE_STRING_H
-
-#ifndef MAIN
+#ifdef HAVE_STRINGS_H
+#  include <strings.h>
+#else
+#  ifdef HAVE_STRING_H
+#    include <string.h>
+#  endif
+#endif
 
 /*F:combine*
 
@@ -86,14 +90,7 @@ ________________________________________________________________
 
 */
 
-#ifndef FUNCPROTO
-int combine(b1, b2, b3, expr1, expr2, expr3, expr4, relop)
-   IBAND b1, b2, b3;
-   int expr1, expr2, expr3, expr4;
-   char *relop;
-#else /* FUNCPROTO */
 int combine(IBAND b1, IBAND b2, IBAND b3, int expr1, int expr2, int expr3, int expr4, char *relop)
-#endif /* FUNCPROTO */
 {
    int x, y, xsize, ysize, a, b, c, d, irelop;
 
@@ -156,92 +153,3 @@ int combine(IBAND b1, IBAND b2, IBAND b3, int expr1, int expr2, int expr3, int e
    }
    return(0);
 }
-
-#endif /* not MAIN */
-
-
-
-/*P:combine*
-
-________________________________________________________________
-
-		combine
-________________________________________________________________
-
-Name:		combine - combine two images
-
-Syntax:		combine <inp1><inp2><outp><expr1><expr2><expr3><expr4><relop>
-
-Description:	The program combines two images into a new image
-                according to the following algorithm:
-
-		| FOR (every pixel) DO
-		|   IF (expr1 RELOP expr2)
-		|   THEN output = expr3
-		|   ELSE output = expr4
-
-                The program requires the following arguments:
-
-		| input 1 - first image to combine
-		| input 2 - second image to combine
-		| output  - name of output image
-		| expr1, expr2, expr3 & expr4 - one of the values:
-		|       -1 for inputimage1[y][x]
-		|       -2 for inputimage2[y][x]
-		|       0..255 - the constant values 0..255
-		| relop - one of (EQ, NE, LT, LE, GT, GE)
-
-Restrictions:	The input images should have UNS_BYTE pixels and
-                equal sizes.
-
-Author:		Tor Lønnestad, BLAB, ifi, UiO.
-
-Examples:	| combine mona.img lena.img res.img -1 -2 50 -2 LT
-
-                -which does the following:
-
-		| FOR (every pixel (x,y)) DO
-		|   IF ( mona[y][x] LT lena[y][x] )
-		|   THEN res[y][x] = 50
-		|   ELSE res[y][x] = lena[y][x]
-
-Id:             $Id$
-________________________________________________________________
-
-*/
-
-#ifdef MAIN
-
-#ifndef FUNCPROTO
-int main(argc,argv)
-int argc;
-char *argv[];
-#else /* FUNCPROTO */
-int main(int argc, char **argv)
-#endif /* FUNCPROTO */
-{
-   IMAGE i1,i2,i3;
-   int e1, e2, e3, e4, n;
-
-   InitMessage(&argc, argv, xite_app_std_usage_text(
-     "Usage: %s <inp1><inp2><outp><expr1><expr2><expr3><expr4><relop>\n"));
-   if (argc == 1) Usage(1, (char*)0);
-   if (argc != 9) Usage(2, "Bad number of arguments\n");
-   Iset_message(TRUE);
-   i1 = Iread_image(argv[1]);
-   i2 = Iread_image(argv[2]);
-   i3 = Icopy_init(i1);
-   e1 = atoi(argv[4]);
-   e2 = atoi(argv[5]);
-   e3 = atoi(argv[6]);
-   e4 = atoi(argv[7]);
-
-   for (n=1; n LE Inbands(i1); INC n)
-     combine(i1[n],i2[n],i3[n],e1,e2,e3,e4,argv[8]);
-
-   Ihistory(i3, argv[0], "");
-   Iwrite_image(i3,argv[3]);
-   return(0);
-}  /* main */
-
-#endif /* MAIN */
