0331ee0a180d901dae75227206bfa6d160a95c51
eran <>
2008-10-14 10:17:06 +0000

    Renamed pseudo2rgb.c to pseudo2rgb_main.c.

--- src/src/convert/pseudo2rgb_main.c
+++ src/src/convert/pseudo2rgb_main.c
@@ -38,100 +38,11 @@ static char *Id = "$Id$, Blab, UiO";
 #include <xite/color.h>
 #include <xite/message.h>
 #include <xite/readarg.h>
-#include XITE_STDIO_H
+#ifdef HAVE_STDIO_H
+#  include <stdio.h>
+#endif
 
 
-
-#ifndef MAIN
-
-/*F:pseudo2rgb*
-
-________________________________________________________________
-
-		pseudo2rgb
-________________________________________________________________
-
-Name:		pseudo2rgb - convert band + colortable to rgb image
-
-Syntax:         | #include <xite/convert.h>
-		|
-                | int pseudo2rgb( IBAND band, IMAGE img,
-                |    Color_tab coltab, int ctlen );
-
-Description:	'pseudo2rgb' converts the input band to a three band 
-                rgb output image according to the specified color table.
-
-Restrictions:	Accepts only UNS_BYTE images. All bands of the output
-                image should have (at least) the same size as the input
-		band.
-
-Return value:   | 0 => ok
-		| 1 => bad pixel type input band
-		| 2 => less than 3 bands in output image
-		| 3 => bad pixel type in output image
-		| 4 => output image less than input band
-		| 5 => ctlen has bad value (<0 or >256)
-
-Author:		Tor Lønnestad, BLAB, ifi, UiO
-
-Revised:        Svein Bøe
-
-Id:		$Id$
-________________________________________________________________
-
-*/
-
-#ifndef FUNCPROTO
-int pseudo2rgb(band, img, coltab, ctlen)
-IBAND band;
-IMAGE img;
-Color_tab coltab;
-int ctlen;
-#else /* FUNCPROTO */
-int pseudo2rgb(IBAND band, IMAGE img, Color_cell *coltab, int ctlen)
-#endif /* FUNCPROTO */
-{
-  int x, y, g, c, xsize, ysize;
-  int ct[256][3];
-  unsigned long pix;
-
-  if (Ipixtyp(band) != Iu_byte_typ)
-    return(Error(1, "Input band must have pixeltype unsigend byte.\n"));
-  if (Inbands(img) < 3)
-    return(Error(2, "Less than 3 bands in output image.\n"));
-  for (c=1; c<=3; c++)
-    if (Ipixtyp(img[c]) != Iu_byte_typ)
-      return(Error(3, "Bad pixeltype in output image.\n"));
-  /* test band sizes */
-  xsize = Ixsize(band);
-  ysize = Iysize(band);
-  for (c=1; c<=3; c++)
-    if ((Ixsize(img[c]) < xsize) || (Iysize(img[c]) < ysize))
-      return(Error(4, "Output image has smaller size than input band.\n"));
-
-  if ((ctlen < 0) || (ctlen > 256))
-    return(Error(5, "Illegal length of colortable.\n"));
-
-  for (g=0; g <= 255; g++)
-    for (c=0; c <= 2; c++)
-      ct[g][c] = 0;
-
-  for (g=0; g < ctlen; g++) {
-    pix = PseudoBasePart(coltab[g].pixel);
-    ct[pix][0] = coltab[g].red/256;
-    ct[pix][1] = coltab[g].green/256;
-    ct[pix][2] = coltab[g].blue/256;
-  }
-
-  for (y=1; y <= ysize; y++)
-    for (x=1; x <= xsize; x++)
-      for (c=0; c <= 2; c++)
-        img[c+1][y][x] = ct[band[y][x]][c];
-
-  return(0);
-}
-
-#endif /* not MAIN */
 
 /*P:pseudo2rgb*
 
@@ -163,15 +74,7 @@ ________________________________________________________________
 
 */
 
-#ifdef MAIN
-
-#ifndef FUNCPROTO
-int main(argc, argv)
-int argc;
-char *argv[];
-#else /* FUNCPROTO */
 int main(int argc, char **argv)
-#endif /* FUNCPROTO */
 {
   IMAGE pseudoImg, rgbImg;
   Color_tab coltab;
@@ -201,5 +104,3 @@ int main(int argc, char **argv)
 
   return(0);
 }
-
-#endif /* MAIN */
