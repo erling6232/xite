a5937d396ba795e3079aad934cd1ce1e19b288f4
eran <>
2008-10-14 10:44:50 +0000

    Renamed lapZeroCross.c to lapZeroCross_main.c.

--- src/src/convolve/lapZeroCross_main.c
+++ src/src/convolve/lapZeroCross_main.c
@@ -35,97 +35,15 @@ static char *Id = "$Id$, Blab, UiO";
 #include <xite/includes.h>
 #include <xite/biff.h>
 #include <stdlib.h>
-#include XITE_STDIO_H
+#ifdef HAVE_STDIO_H
+#  include <stdio.h>
+#endif
 #include <xite/blab.h>
 #include <xite/convolve.h>
 #include <xite/readarg.h>
 #include <xite/message.h>
 
 
-
-#ifndef MAIN
-
-/*F:lapZeroCross*
-
-________________________________________________________________
-
-		lapZeroCross
-________________________________________________________________
-
-Name:		lapZeroCross - find zero crossing of lapacian
-
-Syntax:         | #include <xite/convolve.h>
-		|
-                | BiffStatus lapZeroCross( IBAND iband,
-                |    IBAND oband, int a, int b, int c,
-                |    double scale, double offset, int v );
-
-Description:	'lapZeroCross' finds the zero crossing of the laplacian
-		of the input band. The routine starts by calling the
-		laplace operator, followed by calling the crossing routine.
-
-		The output is zero where the laplacian has no zero
-		crossing, and proportional to the edge strength where
-		the laplacian has a zero crossing.
-
-		The arguments 'a', 'b', 'c', 'scale', 'offset' and 'v' are as
-		described for the laplace operator. Typical values are
-		'a' = 0, 'b' = -1, 'c' = 4, 'scale = 1.0', 'offset' = 127.0.
-
-		The whole algorithm is:
-
-		| <Calculate laplacian>;
-		| FOR <every pixel (x,y)>:
-		| IF <in(x,y) have zero crossing>
-		|   out(x,y) = MAX(ABS(in(x,y)-in(x+1,y)), \\
-		|                  ABS(in(x,y)-in(x,y+1)))
-		| ELSE
-		|   out(x,y) = 0
-
-Restrictions:	'iband' and 'oband' must have pixel type unsigned byte.
-
-See also:	lapZeroCross(1), laplace(3), gradient(3), grad(3), roberts(3),
-                sobel(3), prewitt(3), contour(3), crossing(3)
-
-Return value:	| 0 => ok
-		| 1 => bad pixel type input band
-		| 2 => bad pixel type output band
-		| 3 => some error in crossing
-
-Author:		Tor Lønnestad, BLAB, Ifi, UiO
-
-Id: 		$Id$
-________________________________________________________________
-
-*/
-
-#ifndef FUNCPROTO
-BiffStatus lapZeroCross(iband, oband, a, b, c, scale, offset, v)
-IBAND iband, oband;
-int a, b, c;
-double scale, offset;
-int v;
-#else /* FUNCPROTO */
-BiffStatus lapZeroCross(IBAND iband, IBAND oband, int a, int b, int c, double scale, double offset, int v)
-#endif /* FUNCPROTO */
-{
-  if (Ipixtyp(iband) NE Iu_byte_typ)
-    return(Error(1,
-		 "lapZeroCross: Input pixel type must be unsigned byte.\n"));
-  if (Ipixtyp(oband) NE Iu_byte_typ)
-    return(Error(2,
-		 "lapZeroCross: Output pixel type must be unsigned byte.\n"));
-
-  laplace(iband, oband, a, b, c, scale, offset, v);
-
-  if (crossing(oband, oband, (int)offset))
-    return(Error(3,
-		 "lapZeroCross: Error in crossing().\n"));
-
-  return(0);
-}
-
-#endif /* not MAIN */
 
 /*P:lapZeroCross*
 
@@ -191,15 +109,7 @@ ________________________________________________________________
 
 */
 
-#ifdef MAIN
-
-#ifndef FUNCPROTO
-int main(argc, argv)
-int argc;
-char *argv[];
-#else /* FUNCPROTO */
 int main(int argc, char **argv)
-#endif /* FUNCPROTO */
 {
   IMAGE img1, img2;
   double scale, offset;
@@ -239,5 +149,3 @@ int main(int argc, char **argv)
 
   return(0);
 }
-
-#endif /* MAIN */
