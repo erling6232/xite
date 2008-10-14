55f2c5698e3f76c20000b591acc25c604a5497a4
eran <>
2008-10-14 09:11:17 +0000

    Renamed contour.c to contour_main.c.

--- src/src/contour/contour_main.c
+++ src/src/contour/contour_main.c
@@ -47,95 +47,6 @@ static char *Id = "$Id$, Blab, UiO";
 # define MIN(a,b) ((a)<(b) ? a : b)
 #endif
 
-#ifndef MAIN
-
-/*F:contour*
-
-________________________________________________________________
-
-		contour
-________________________________________________________________
-
-Name:		contour - create a contour map
-
-Syntax:         | #include <xite/contour.h>
-                |
-                | int contour( IBAND input, IBAND output,
-                |    int eqd, int cpv, int bpv );
-
-Description:    Creates a contour map of the input image, with
-                equidistance 'eqd'. Contour pixels receive the value 'cpv',
-		contour pixel value, and background pixels receive the value
-		'bpv', background pixel value. Contours should
-		really be drawn 'between' the pixels, but are
-		shifted half a pixel to the left or half a pixel
-		upward to hit a pixel.
-
-Restrictions:   'input' and 'output' must have pixel type unsigned byte.
-                'eqd' must be in [2, 255].
-
-Return value:   | 0 => ok
-                | 2 => bad pixel type input
-		| 3 => bad pixel type output
-		| 4 => bad eqd value (less than 2 or greater than 255)
-
-See also:       contour(1), crossing(3)
-
-Author:		Tor Lønnestad, BLAB, Ifi, UiO.
-
-Id:             $Id$
-________________________________________________________________
-
-*/
-
-int contour(IBAND input, IBAND output, int eqd, int cpv, int bpv)
-{
-   int x, y, xsize, ysize, i, mapval;
-   UNS_BYTE map[256];
-
-   if (Ipixtyp(input) NE Iu_byte_typ)
-     return(Error(2, "contour: Input pixel type must be unsigned byte.\n"));
-   if (Ipixtyp(output) NE Iu_byte_typ)
-     return(Error(3, "contour: Output pixel type must be unsigned byte.\n"));
-   if ((eqd LT 2) OR (eqd GT 255))
-     return(Error(4, "contour: bad eqd argument value.\n"));
-
-   if (cpv LT 0)   cpv = 0;
-   if (cpv GT 255) cpv = 255;
-   if (bpv LT 0)   bpv = 0;
-   if (bpv GT 255) bpv = 255;
-
-   for (i=0; i LE 255; ++ i) map[i] = i/eqd;
-
-   xsize = MIN(Ixsize(input), Ixsize(output));
-   ysize = MIN(Iysize(input), Iysize(output));
-
-   FOR (y=1; y LT ysize; ++ y)
-     FOR (x=1; x LT xsize; ++ x)
-       mapval = map[input[y][x]];
-       if ((mapval NE map[input[y][x+1]])
-         OR (mapval NE map[input[y+1][x]]))
-         output[y][x] = cpv; /* contour */
-       else output[y][x] = bpv; /* background */
-     ENDFOR;  /* all x */
-     output[y][xsize] =
-       (map[input[y][x]] NE map[input[y+1][x]]) ? cpv : bpv;
-   ENDFOR;  /* all y */
-
-   for (x=1; x LT xsize; ++ x)
-     output[ysize][x] = 
-       (map[input[ysize][x]] NE map[input[ysize][x+1]]) ? cpv : bpv;
-
-   output[ysize][xsize] = bpv;
-
-   return(0);
-
-}  /* contour */
-
-#endif /* MAIN */
-
-
-
 /*P:contour*
 
 ________________________________________________________________
@@ -173,8 +84,6 @@ ________________________________________________________________
 
 */
 
-#ifdef MAIN
-
 int main(int argc, char **argv)
 {
    IMAGE i1, i2;
@@ -210,5 +119,3 @@ int main(int argc, char **argv)
 
    return(0);
 }
-
-#endif /* MAIN */
