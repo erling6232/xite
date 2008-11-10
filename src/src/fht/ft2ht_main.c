c66d4ee22371d93143422265051b72ba62b5a79f
eran <>
2008-11-10 09:30:13 +0000

    Renamed ft2ht.c to ft2ht_main.c.

--- src/src/fht/ft2ht_main.c
+++ src/src/fht/ft2ht_main.c
@@ -46,72 +46,6 @@ static char *Id = "$Id$, Blab, UiO";
 
 
 
-#ifndef MAIN
-
-/*F:ft2ht*
-
-________________________________________________________________
-
-		ft2ht
-________________________________________________________________
-
-Name:		ft2ht - 2d fourier to hartley conversion
-
-Syntax:         | #include <xite/fht.h>
-		|
-                | int ft2ht( IC_BAND inband, IR_BAND outband );
-
-Description:    'ft2ht' converts a Fourier transform to a Hartley transform.
-                The Hartley transform is given by the Fourier transform
-		as follows:
-
-		| H(u,v) = Re{F(u,v)} - Im{F(u,v)}
-		|        = H_e(u,v) + H_o(u,v)
-		where H_e and H_o are the even and odd parts of H, i.e.
-		| H_e(u,v) = (H(u,v) + H(-u,-v))/2
-		| H_o(u,v) = (H(u,v) - H(-u,-v))/2
-
-		For images with horizontal and vertical even symmetry,
-		H_o(u,v) equals zero, so that
-		| H(u,v) = F(u,v)
-
-Restrictions:	bands should have equal size, be quadratic, and have
-                size some power of two. Currently, only single precision
-		float and complex values are accepted.
-
-See also:	ft2ht(1), fft2d(3), fht2d(3), ht2ft(3)
-
-Return value:   | 0 => ok
-                | 1 => bad pixel type input
-		| 2 => bad pixel type output
-		| 3 => bad sizes
-
-Author:		Tor Lønnestad, BLAB, Ifi, UiO
-
-Id: 		$Id$
-________________________________________________________________
-
-*/
-
-int ft2ht(IC_BAND inband, IR_BAND outband)
-{
-  int x, y, xsize, ysize;
-
-  if (Ipixtyp((IBAND) inband) NE Icomplex_typ) return(1);
-  if (Ipixtyp((IBAND) outband) NE Ireal_typ) return(2);
-  xsize = Ixsize((IBAND) inband);
-  ysize = Iysize((IBAND) inband);
-  if (xsize NE Ixsize((IBAND) outband)) return(3);
-  if (ysize NE Iysize((IBAND) outband)) return(3);
-
-  for (y=1; y LE ysize; y++)
-    for (x=1; x LE xsize; x++)
-      outband[y][x] = inband[y][x].re - inband[y][x].im;
-  return(0);
-}
-
-#endif /* not MAIN */
-
 /*P:ft2ht*
 
 ________________________________________________________________
@@ -162,8 +96,6 @@ ________________________________________________________________
 
 */
 
-#ifdef MAIN
-
 int main(int argc, char **argv)
 {
   IMAGE img1, img2;
@@ -209,5 +141,3 @@ int main(int argc, char **argv)
 
   return(0);
 }
-
-#endif /* MAIN */
