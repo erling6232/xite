be76b83a291853eec2352cd549bc429c622936f8
eran <>
2008-11-10 09:57:50 +0000

    Updated.

--- src/lib/fractal/mkFractalSra.c
+++ src/lib/fractal/mkFractalSra.c
@@ -40,7 +40,9 @@ static char *Id = "$Id$, Blab, UiO";
 #include <xite/fractal.h>
 #include <xite/readarg.h>
 #include <xite/utils.h>
-#include XITE_RANDOM_H
+#ifdef HAVE_STDLIB_H
+#  include <stdlib.h>
+#endif
 #ifdef HAVE_STDIO_H
 #  include <stdio.h>
 #endif
@@ -56,7 +58,6 @@ static char *Id = "$Id$, Blab, UiO";
 # define MIN(a,b) (((a)<(b)) ? (a) : (b))
 #endif
 
-#ifndef MAIN
 
 #define f3(std,x0,x1,x2)    (UNS_BYTE)(((x0)+(x1)+(x2)     )/3.0 + gaussRandom(0.5, std));
 #define f4(std,x0,x1,x2,x3) (UNS_BYTE)(((x0)+(x1)+(x2)+(x3))/4.0 + gaussRandom(0.5, std));
@@ -221,117 +222,3 @@ IBAND mkFractalSra(double D, int xpow, int ypow, double mean, double cstdev, dou
 
   return(band);
 }
-
-#endif /* not MAIN */
-
-/*P:mkFractalSra*
-
-________________________________________________________________
-
-		mkFractalSra
-________________________________________________________________
-
-Name:		mkFractalSra - make fractal brownian motion noise
-
-Syntax:		mkFractalSra [-sw] <output image> <D> [<xsize> [<ysize>]]
-
-Description:    Generates fractional brownian motion noise
-                with fractal dimension D. The algorithm is known
-		as "Successive Random Addition"
-
-                |   output image - name of generated image
-                |   D            - fractal dimension between 2. and 3.
-		|   xsize        - xsize of synthesized image
-		|                  (default 512)
-		|   ysize        - ysize of synthesized image
-		|                  (default xsize)
-		Switches:
-		|   -mean        - mean pixel value for corners
-		|                  (default 128.0)
-		|   -cstd        - standard deviation for corners
-		|                  (default 5.0)
-		|   -std         - init. st. dev. for other pixels
-		|                  (default 25.0)
-
-		Note that 'mean' far from 128 combined with large
-		standard deviations will give a lot of over/under-flows,
-		and a very poor result. 'cstdev' should be small to avoid
-		"tilting" the whole image.
-
-See also:	mkGauss(1), mkFractalSra(3)
-
-Author:		Håvard Iversen, BLAB, Ifi, UiO
-
-Modifyed by:    Tor Lønnestad, BLAB, Ifi, UiO
-
-Reference:      &'Fournier, A., Fussel, D., and Carpenter, L.'
-                "Computer rendering of stochastic models",
-		Commun. ACM 25, 6 (June 1982), 372-382.
-
-Example:        | mkFractalSra -mean 100.0 fractal.img 2.5 200 400 
-
-Id: 		$Id$
-________________________________________________________________
-
-*/
-
-#ifdef MAIN
-
-#ifndef FUNCPROTO
-int main(argc,argv)
-int argc;
-char *argv[];
-#else /* FUNCPROTO */
-int main(int argc, char **argv)
-#endif /* FUNCPROTO */
-{
-  IBAND b;
-  IMAGE i;
-  double D, mean, cstdev, stdev;
-  int xsize, ysize, size, xpow, ypow;
-  char arg[80];
-
-  Iset_message(TRUE);
-  Iset_abort(TRUE);
-  InitMessage(&argc, argv, xite_app_std_usage_text(
-    "Usage: %s [<options>] <file> <D> [<xsize> [<ysize>]] \n\
-       where <D> is fractal dimension between 2.0 and 3.0 \n\
-       and where <options> are \n\
-	-mean <mean> - mean pixel value for corners (default 128.0)\n\
-	-cstd <cstd> - standard deviation for corners (default 5.0)\n\
-	-std  <std>  - init. st. dev. for other pixels (default 25.0)\n"));
-
-  if (argc == 1) Usage(1, NULL);
-
-  mean   = read_dswitch(&argc, argv, "-mean", 128.0);
-  cstdev = read_dswitch(&argc, argv, "-cstd",   5.0);
-  stdev  = read_dswitch(&argc, argv, "-std",   25.0);
-
-  if ((argc < 3) || (argc > 5)) Usage(1, "Illegal number of arguments.\n");
-
-  D = atof(argv[2]);
-  xsize = (argc > 3) ? (atoi(argv[3])) : 512;
-  ysize = (argc > 4) ? (atoi(argv[4])) : xsize;
-  if ((D < 2.0) || (D > 3.0)) Error(2, "D should be between 2.0 and 3.0");
-  if (xsize < 3) Error(2, "xsize too small");
-  if (ysize < 3) Error(2, "ysize too small");
-
-  for(xpow=1,size=2; (size+1)<xsize; xpow++, size<<=1);  
-  for(ypow=1,size=2; (size+1)<ysize; ypow++, size<<=1);  
-
-  b = mkFractalSra(D, xpow, ypow, mean, cstdev, stdev);
-  if (! b) Error(2, "could not allocate memory");
-  i = Init_image(1, "Fractal image");
-  i[1] = Imake_subband(b, 1, 1, xsize, ysize);
-
-  sprintf(arg, "  D: %s  mean:%.2f  cstd:%.2f  std:%.2f",
-	  argv[2], mean, cstdev, stdev);
-  Ihistory(i, argv[0], arg);
-  Iwrite_image(i,argv[1]);
-  Idel_image(&i);
-  Idel_band(&b);
-
-  return(0);
-}
-
-#endif /* MAIN */
