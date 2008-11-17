f3987768a492a77cd9abcbac0d74f412b4928114
eran <>
2008-11-17 10:48:45 +0000

    Updated.

--- src/include/Xfwf/Directory.h
+++ src/include/Xfwf/Directory.h
@@ -39,10 +39,20 @@
 #endif
 
 #ifdef HAVE_DIRENT_H
-#include <dirent.h>
+# include <dirent.h>
+# define NAMLEN(dirent) strlen ((dirent)->d_name)
 #else
-#include <sys/dir.h>
-#define	dirent direct
+# define dirent direct
+# define NAMLEN(dirent) ((dirent)->d_namlen)
+# ifdef HAVE_SYS_NDIR_H
+#  include <sys/ndir.h>
+# endif
+# ifdef HAVE_SYS_DIR_H
+#  include <sys/dir.h>
+# endif
+# ifdef HAVE_NDIR_H
+#  include <ndir.h>
+# endif
 #endif
 
 #ifndef _SYS_NAME_MAX
