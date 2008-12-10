70af68d988ede0cb37a996dfc3f97d639a879663
eran <>
2008-12-10 10:00:54 +0000

    Renamed gradInv.c to gradInv_main.c.

--- src/src/gradInv/gradInv_main.c
+++ src/src/gradInv/gradInv_main.c
@@ -51,103 +51,6 @@ static char *Id = "$Id$, Blab, UiO";
 # define MIN(a,b) ((a)<(b) ? a : b)
 #endif
 
-#ifndef MAIN
-          
-/*F:gradInv*
-
-________________________________________________________________
-
-		gradInv
-________________________________________________________________
-
-Name:		gradInv - gradient invers noise reduction
-
-Syntax:         | #include <xite/gradInv.h>
-		|
-                | int gradInv( IBAND input, IBAND output, int dx,
-                |    int dy );
-
-Description:    'gradInv' performs noise reduction by gradient
-                invers smoothing.
-
-		'dx' is the horizontal window size.
-		'dy' is the vertical window size.
-
-		The border is filtered as good as possible.
-
-Return value:   | 0 => ok
-                | 1 => bad pixel type input
-		| 2 => bad pixel type output
-		| 3 => bad dx value (less than 2)
-		| 4 => bad dy value (less than 2)
-
-See also:       gradInv(1)
-
-References:     &Wang, Vagnucci and Li,
-                "Gradient Inverse Weighted Smoothing Scheme and the Evaluation
-		of Its Performance",
-		Comp. Graph. and Im. Proc. '15', 167-181 (1981).
-
-Author:		Tor Lønnestad, BLAB, Ifi, UiO.
-
-Id:             $Id$
-________________________________________________________________
-
-*/
-
-int gradInv(IBAND input, IBAND output, int dx, int dy)
-{
-   int x, y, xx, yy, xstart, ystart, xstop, ystop, dxhalf, dyhalf;
-   int xsize, ysize, grad;
-   float sumGrad, sumGradPixval, invgrad;
-
-   if (Ipixtyp(input) NE Iu_byte_typ)
-     return(Error(1, "gradInv: Input pixel type must be unsigned byte.\n"));
-   if (Ipixtyp(output) NE Iu_byte_typ)
-     return(Error(2, "gradInv: Output pixel type must be unsigned byte.\n"));
-   if (dx LT 2)
-     return(Error(3, "gradInv: Horizontal window size is too small.\n"));
-   if (dy LT 2)
-     return(Error(4, "gradInv: Vertical window size is too small.\n"));
-
-   xsize = MIN(Ixsize(input), Ixsize(output));
-   ysize = MIN(Iysize(input), Iysize(output));
-   dxhalf = dx/2; dx = dxhalf*2 + 1;
-   dyhalf = dy/2; dy = dyhalf*2 + 1;
-
-   FOR (y=1; y LE ysize; INC y)
-     ystart = MAX(1, y-dyhalf);
-     ystop = MIN(ysize, y+dyhalf);
-     FOR (x=1; x LE xsize; INC x)
-       xstart = MAX(1, x-dxhalf);
-       xstop = MIN(xsize, x+dxhalf);
-       sumGrad = 0.0; sumGradPixval = 0.0;
-       FOR (yy=ystart; yy LE ystop; INC yy)
-         FOR (xx=xstart; xx LE xstop; INC xx)
-           if ((xx EQ x) AND (yy EQ y)) continue;
-           grad = input[yy][xx] - input[y][x];
-           IF (grad LT 0) THEN invgrad = -1.0/grad;
-           ELSEIF (grad EQ 0) THEN invgrad = 2.0;
-           ELSE invgrad = 1.0/grad;
-           ENDIF;
-           sumGrad += invgrad;
-           sumGradPixval += invgrad*input[yy][xx];
-         ENDFOR;
-       ENDFOR;
-
-       output[y][x] = 
-        (int)(((float)input[y][x] + sumGradPixval/sumGrad) * 0.5 + 0.5);
-     ENDFOR;  /* all x */
-   ENDFOR;  /* all y */
-
-   return(0);
-
-}  /* gradInv */
-
-#endif /* not MAIN */
-
-
-
 /*P:gradInv*
 
 ________________________________________________________________
@@ -188,8 +91,6 @@ ________________________________________________________________
 
 */
 
-#ifdef MAIN
-
 int main(int argc, char **argv)
 {
    IMAGE input, output;
@@ -220,5 +121,3 @@ int main(int argc, char **argv)
 
    return(0);
 }
-
-#endif /* MAIN */
