e20e7ba99c44b9a067bb902c87e647cd62929148
eran <>
2008-12-15 10:38:05 +0000

    Updated.

--- src/lib/matlab/biff2matlab.c
+++ src/lib/matlab/biff2matlab.c
@@ -52,8 +52,6 @@ static char *biff2matlab_error_s[] = {
 
 
 
-#ifndef MAIN
-
 /*F:band2matlab*
 
 ________________________________________________________________
@@ -426,93 +424,3 @@ int saveBiffAsMatlab(IMAGE img, char *matFileName)
     return(Iok);
 
 } /* saveBiffAsMatlab */
-
-#endif /* not MAIN */
-
-
-
-/*P:biff2matlab*
-
-________________________________________________________________
-
-		biff2matlab
-________________________________________________________________
-
-Name:		biff2matlab - Convert from XITE BIFF image file format to
-                MATLAB matrix file format.
-
-Syntax:		biff2matlab <biffFile> <matFile>
-
-Description:    Convert from XITE BIFF image file format to MATLAB version 5
-                MAT-file format. Each band may consist of any of the
-		following pixel	types: UNS_BYTE, SIGN_BYTE, UNS_SHORT,
-		SIGN_SHORT, INTEGER, REAL, COMPLEX, DOUBLE, DCOMPLEX.
-
-		"-", "" or "-0" can be used for 'stdin'. The input filename
-		may be followed by band specifications, as in file:3 for the
-		third band (see 'Iread_image(3)').
-		
-		If the BIFF image has a non-empty title, the name (names)
-		is (are) generated from it (with a filename extension for
-		multiband images). If the BIFF image has no non-empty title,
-		the name(s) is (are) created from the MATLAB matrix filename
-		(and band number). With an empty filename, the name is not
-		set.
-
-		A MATLAB matrix name must start with a letter and only
-		consist of letters, digits or underscores (a total of
-		maximum mxMAXNAM characters). An illegal initial character is
-		replaced by the letter "a", any other character is replaced
-		by an underscore.
-
-		If the MATLAB matrix file exists, the matrix is appended to
-		the file (unless a matrix with the same name already exists,
-		in which case the new matrix replaces the old).
-
-See also:	'matlab2biff(1)', 'Iread_image(5)'
-
-Return value:   | 0 => OK
-                | 1 => Usage message
-                | 2 => Illegal command line
-		| 3 => Failure in creating MATLAB matrix
-		| 4 => Failure in converting BIFF band to MATLAB matrix
-                | 5 => Failure in opening MATLAB matrix file
-		| 6 => Failure in writing MATLAB matrix to file
-
-Author:		Svein Bøe, BLAB, Ifi, UiO
-
-Id: 		$Id$
-________________________________________________________________
-
-*/
-
-#ifdef MAIN
-
-int main(int argc, char **argv)
-{
-    IMAGE img;
-    int status;
-
-    /* Initialize XITE program */
-
-    Iset_message(1);
-    Iset_abort(1);
-    InitMessage(&argc, argv, xite_app_std_usage_text(
-      "Usage: %s <biffFile> <matlabFile>\n\
-        '-' can be used for stdin.\n"));
-
-    /* stdin if name is "-", "" or "-0" (XITE standard) */
-
-    if (argc == 1) Usage(1, NULL);
-    if (argc != 3) Usage(2, "Illegal number of arguments.\n");
-
-    img = Iread_image(argv[1]);
-
-    if ((status = saveBiffAsMatlab(img, argv[2])) != Iok)
-      Error(status, biff2matlab_error_s[status]);
-
-    return(0);
-}
-
-#endif /* MAIN */
-
--- src/lib/matlab/matlab2biff.c
+++ src/lib/matlab/matlab2biff.c
@@ -56,8 +56,6 @@ static char *matlab2biff_error_s[] = {
 
 
 
-#ifndef MAIN
-
 /*F:matlab2band*
 
 ________________________________________________________________
@@ -420,105 +418,3 @@ int readMatrices(mxArray ***ptrMatrices, long *nMat, char *matNames[], long nNam
   else return(Iok);
 
 } /* readMatrices */
-
-#endif /* not MAIN */
-
-
-
-/*P:matlab2biff*
-
-________________________________________________________________
-
-		matlab2biff
-________________________________________________________________
-
-Name:		matlab2biff - Convert from MATLAB MAT-file format to
-                XITE BIFF image file format.
-
-Syntax:		matlab2biff <MAT-file> <BIFF-file> [-s <matrixName>] ...
-
-Description:    Convert from MATLAB MAT-file format to XITE BIFF image file
-                format. Only numeric matrices are converted. If more than
-		one matrix, they are all converted into the same BIFF file.
-		Default is all numeric matrices. The XITE BIFF image bands
-		will get pixel type double or double complex.
-
-		"-", "" or "-1" can be used for 'stdout'. The output filename
-		may be followed by band specifications, as in "file:3" for the
-		third band (see 'Iwrite_image(3)').
-
-		The BIFF image title will be the same as the name of the first
-		MATLAB matrix with a non-empty name string, or else the BIFF
-		filename (if not "-" or "" or "-1" for 'stdout'), or the
-		empty string.
-
-		Will read both MATLAB version 4 and version 5 MAT-files.
-
-Restrictions:   Will not read sparse matrices.
-
-See also:	'biff2matlab(1)', 'Iwrite_image(5)'
-
-Return value:   | 0 => OK
-                | 1 => Usage message
-                | 2 => Illegal command line
-                | 3 => Failure in opening the MAT-file.
-		| 4 => Failure in reading the MAT-file directory.
-		| 5 => Did not find all requested matrices in file.
-                | 6 => Failure in initializing BIFF image.
-		| 7 => Failure in creating BIFF image band.
-		| 8 => Failure in converting MATLAB matrix to
-		|      XITE BIFF band.
-		| 9 => Failure in opening BIFF image file.
-		| 10 => Failure in writing BIFF image band to file.
-
-Author:		Svein Bøe, BLAB, Ifi, UiO
-Id: 		$Id$
-________________________________________________________________
-
-*/
-
-#ifdef MAIN
-
-int main(int argc, char **argv)
-{
-    mxArray **matrices;
-    char *matNames[100], *name;
-    long nMat, nNames;
-    int status;
-
-    /* Initialize XITE program */
-
-    Iset_message(1);
-    Iset_abort(1);
-    InitMessage(&argc, argv, xite_app_std_usage_text(
-      "Usage: %s <matFile> <biffFile> [-s <matrixName>] ... \n\
-        '-' can be used for stdout.\n"));
-
-    /* stdout if name is "-", "" or "-1" (XITE standard) */
-
-    if (argc == 1) Usage(1, NULL);
-    if ((argc % 2) != 1) Usage(2, "Illegal number of arguments.\n");
-
-    nNames = 0;
-    while (argc > 3) {
-      if (strlen(name = read_switch(&argc, argv, "-s", 1, "")) > 0)
-	matNames[nNames++] = name;
-      else break;
-    }
-
-    if ((argc % 2) != 1) Usage(2, "Illegal number of arguments.\n");
-
-    /* nNames == 0: Read all numeric matrices (default) */
-    if (( status = readMatrices(&matrices, &nMat, matNames, nNames, argv[1]))
-	!= Iok)
-      Error(status + 2, matlab2biff_error_s[status+2]);
-
-    if ((status = saveMatlabAsBiff((const mxArray **) matrices, nMat,
-				   argv[2], argv[0]))
-	!= Iok)
-      Error(status + 5, matlab2biff_error_s[status+5]);
-
-    return(0);
-}
-
-#endif /* MAIN */
