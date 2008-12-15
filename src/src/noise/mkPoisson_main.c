8538981c1a4b2e901fc7642f497792411d2d23c0
eran <>
2008-12-15 13:25:12 +0000

    Renamed mkPoisson.c to mkPoisson_main.c.

--- src/src/noise/mkPoisson_main.c
+++ src/src/noise/mkPoisson_main.c
@@ -45,93 +45,6 @@ static char *Id = "$Id$, Blab, UiO";
 
 
 
-#ifndef MAIN
-
-/*F:mkPoisson*
-
-________________________________________________________________
-
-		mkPoisson
-________________________________________________________________
-
-Name:		mkPoisson - make a band with Poisson random noise.
-
-Syntax:         | #include <xite/noise.h>
-                |
-                | int mkPoisson( double alfa, IBAND output );
-
-Description:    'mkPoisson' inserts random noise with Poisson distribution
-                in band output. The distribution has mean value 'alfa' and
-                variance 'alfa'. If resulting value is larger than 255, output 
-                is set to 255, and if resulting value is less than 0, output 
-                is set to 0.
-
-Restrictions:   Accepts only unsigend byte pixeltype.
-
-Return value:   | 0 => ok
-                | 1 => Bad pixel type
-
-Reference:      J. Banks: Discrete-event system simulation.
-		 
-Author:		Helene Schulerud, BLAB, Ifi, UiO
-
-Id:             $Id$
-________________________________________________________________
- 
-*/
-
-int mkPoisson(double alfa, IBAND output)
-{
-  int x, y, xsize, ysize;
-  int n;
-  long rl;
-  double rl2,p,ek;
- 
-  if (Ipixtyp(output) NE Iu_byte_typ)
-    return(Error(1, "mkPoisson: Bad pixel type\n"));
-
-  xsize = Ixsize(output);
-  ysize = Iysize(output);
-
-  srandom(time(0)); /* random initialization of random generator */
-
-  for ( y=1; y<=ysize; y++ )
-    for ( x=1; x<=xsize; x++ )
-      {
-	n=0;
-	p=1;
-	rl=random();
-	rl2=(double)rl/RANDOM_MAX;
-	p=p*rl2;
-        ek = (double)(exp(-alfa));
-	while ( p >= ek )
-	  {
-	    n++;
-	    rl=random();
-	    rl2=(double)rl/RANDOM_MAX;
-	    p=p*rl2;
-	    ek = (exp(-alfa));
-	  }
-
-	if (n<=255)
-	  output[y][x] = n;
-	else if (n<=0)
-	   output[y][x] = 0;
-	else
-	  output[y][x] = 255; 
-
-      }
- 
-  return(0);
-
-}
-
-#endif /* not MAIN */
-
-
-
-#ifdef MAIN
-
 /*P:mkPoisson*
 
 ________________________________________________________________
@@ -199,5 +112,3 @@ int main(int argc, char **argv)
 
   return(0);
 }
-
-#endif /* MAIN */
