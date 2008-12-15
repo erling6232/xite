5b070a4f8c51374b8d6c0066e3f6c61a366f4348
eran <>
2008-12-15 13:23:32 +0000

    Renamed addGauss.c to addGauss_main.c.

--- src/src/noise/addGauss_main.c
+++ src/src/noise/addGauss_main.c
@@ -42,72 +42,6 @@ static char *Id = "$Id$, Blab, UiO";
 #include XITE_STDIO_H
 #include XITE_TIME_H
 
-#ifndef MAIN
-
-#ifndef MIN
-# define MIN(a,b) (((a) < (b)) ? (a) : (b))
-#endif
-
-
-/*F:addGauss*
-
-________________________________________________________________
-
-		addGauss
-________________________________________________________________
-
-Name:		addGauss - add gaussian random noise to a band
-
-Syntax:         | #include <xite/noise.h>
-		|
-                | BiffStatus addGauss( IBAND b1, IBAND b2,
-                |    double my, double sigma );
-Description:    Adds random noise with Gaussian distribution to
-                band b1, places the result in band b2. The
-                distribution has mean value my and standard
-                deviation sigma. If resulting value is larger
-                than 255 output is set to 255, and if resulting
-                value is less than 0 output is set to 0.
-
-Return value:   | 0 => ok
-                | 1 => Bad input pixel type
-                | 2 => Bad output pixel type
-
-Author:		Tor L|nnestad, BLAB, IfI, UiO
-
-Id:             $Id$
-________________________________________________________________
-
-*/
-
-BiffStatus addGauss(IBAND b1, IBAND b2, double my, double sigma)
-{
-  int x, y, xsize, ysize, tmp;
-
-  if (Ipixtyp(b1) NE Iu_byte_typ)
-    return(Error(1, "addGauss: Bad input pixel type\n"));
-  if (Ipixtyp(b2) NE Iu_byte_typ)
-    return(Error(2, "addGauss: Bad output pixel type\n"));
-  xsize = MIN(Ixsize(b1),Ixsize(b2));
-  ysize = MIN(Iysize(b1),Iysize(b2));
-  my += 0.5; /* due to conversion to int later on */
-
-  srandom(time(0)); /* random initialization of random generator */
-  FOR (y=1; y LE ysize; INC y)
-    FOR (x=1; x LE xsize; INC x)
-      tmp = (int)((double)b1[y][x] + gaussRandom(my,sigma));
-      if (tmp LT 0) b2[y][x] = 0;
-      else if (tmp GT 255) b2[y][x] = 255;
-      else b2[y][x] = (UNS_BYTE)tmp;
-    ENDFOR;
-  ENDFOR;
-  return(0);
-} /* int addGauss */
-
-#endif /* not MAIN */
-
-
-
 /*P:addGauss*
 
 ________________________________________________________________
@@ -140,9 +74,6 @@ ________________________________________________________________
 
 */
 
-
-#ifdef MAIN
-
 int main(int argc, char **argv)
 {
   IMAGE i1, i2;
@@ -172,5 +103,3 @@ int main(int argc, char **argv)
   Iwrite_image(i2, argv[2]);
   return(0);
 }
-
-#endif /* MAIN */
