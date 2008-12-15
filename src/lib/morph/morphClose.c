f7b3f72b26d915892781d2c89e1ea9e8e675ae36
eran <>
2008-12-15 13:03:25 +0000

    Copied morphClose.c here from /hus/home/eran/src/Xite/2008-10-12/xite/src/morph.

---
+++ src/lib/morph/morphClose.c
@@ -0,0 +1,108 @@
+
+/*
+
+________________________________________________________________
+
+        
+        $Id$
+        Copyright 1992, Blab, UiO
+        Image processing lab, Department of Informatics
+        University of Oslo
+        E-mail: blab@ifi.uio.no
+________________________________________________________________
+  
+  Permission to use, copy, modify and distribute this software and its
+  documentation for any purpose and without fee is hereby granted, 
+  provided that this copyright notice appear in all copies and that 
+  both that copyright notice and this permission notice appear in supporting
+  documentation and that the name of B-lab, Department of Informatics or
+  University of Oslo not be used in advertising or publicity pertaining 
+  to distribution of the software without specific, written prior permission.
+
+  B-LAB DISCLAIMS ALL WARRANTIES WITH REGARD TO THIS SOFTWARE, INCLUDING ALL
+  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS, IN NO EVENT SHALL B-LAB
+  BE LIABLE FOR ANY SPECIAL, INDIRECT OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+  WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION
+  OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN 
+  CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+
+*/
+
+static char *Id = "$Id$, Blab, UiO";
+
+#include <xite/includes.h>
+#include <xite/biff.h>
+#include <stdlib.h>
+#include <xite/blab.h>
+#include <xite/convert.h>
+#include <xite/message.h>
+#include <xite/morph.h>
+#include <xite/readarg.h>
+
+
+
+#ifndef MAIN
+
+int morphClose(IBAND inband, IBAND outband, ISS_BAND B)
+{
+  int status;
+  IBAND tmpband;
+
+  tmpband = Imake_band(Iu_byte_typ, Ixsize(inband), Iysize(inband));
+  status = morphDilate(inband, tmpband, B);
+  if (status) return(Error(status, "morphOpen: Error in morphDilate\n"));
+  status = morphErode(tmpband, outband, B);
+  if (status) return(Error(status, "morphOpen: Error in morphErode\n"));
+  Idel_band(&tmpband);
+
+  return(0);
+}
+
+#endif /* not MAIN */
+
+
+
+#ifdef MAIN
+
+int main(int argc, char **argv)
+{
+  IMAGE inimage, outimage;
+  ISS_IMAGE B;
+  ISS_BAND B_band;
+  int bn;
+  char *args;
+  char *a,*b;
+
+  InitMessage(&argc,argv, xite_app_std_usage_text(
+    "Usage: %s <inimage> <outimage> -a <ascii struct elem>\n\
+       or: %s <inimage> <outimage> -b <biff struct elem>\n"));
+
+  Iset_message(TRUE);
+
+  if (argc == 1) Usage(1, NULL);
+  args = argvOptions(argc, argv);
+
+  a = read_switch(&argc, argv, "-a", 1, NULL);
+  b = read_switch(&argc, argv, "-b", 1, NULL);
+
+  if (argc NE 3) Usage(2,"Illegal number of arguments.\n");
+
+  inimage  = Iread_image(argv[1]);
+
+  if (a) ascii2biff((IBAND *)&B_band, a, Is_short_typ);
+  else if (b) {
+    B = (ISS_IMAGE) Iread_image(b);
+    B_band = B[1];
+  } else Usage(3, "No structuring element.\n");
+
+  outimage = Icopy_init(inimage);
+  for (bn=1; bn LE Inbands(inimage); bn++)
+    morphClose(inimage[bn], outimage[bn], B_band);
+
+  Ihistory(outimage, argv[0], args);
+  Iwrite_image(outimage, argv[2]);
+
+  return(0);
+}
+
+#endif /* MAIN */
