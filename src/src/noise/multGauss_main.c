d6b1146b260f4f35f71a86dce432a83bec762e50
eran <>
2008-12-15 13:24:19 +0000

    Renamed multGauss.c to multGauss_main.c.

--- src/src/noise/multGauss_main.c
+++ src/src/noise/multGauss_main.c
@@ -40,76 +40,6 @@ static char *Id = "$Id$, Blab, UiO";
 #include <xite/message.h>
 #include <xite/utils.h>
 
-#ifndef MAIN
-
-#ifndef MIN
-# define MIN(a,b) (((a) < (b)) ? (a) : (b))
-#endif
-
-
-/*F:multGauss*
-
-________________________________________________________________
-
-		multGauss
-________________________________________________________________
-
-Name:		multGauss - multiply band with random gaussian noise
-
-Syntax:         | #include <xite/noise.h>
-		|
-                | int multGauss( IBAND b1, IBAND b2, double my,
-                |    double sigma );
-Description:    'multGauss' multiplies random noise with Gaussian
-                distribution to band 'b1', and places the result in
-		band 'b2'. The distribution has mean value my and 
-                standard deviation sigma. If resulting value is larger
-                than 255 output is set to 255, and if resulting
-                value is less than 0 output is set to 0.
-
-Return value:   | 0 => ok
-                | 1 => Bad input pixel type
-                | 2 => Bad output pixel type
-		| 3 => Bad my value
-		| 4 => Bad sigma value
-
-Author:		Tor L|nnestad, BLAB, ifi, UiO
-
-Id:             $Id$
-________________________________________________________________
-
-*/
-
-int multGauss(IBAND b1, IBAND b2, double my, double sigma)
-{
-  int x, y, xsize, ysize, tmp;
-
-  if (Ipixtyp(b1) NE Iu_byte_typ)
-    return(Error(1, "multGauss: Bad input pixel type\n"));
-  if (Ipixtyp(b2) NE Iu_byte_typ)
-    return(Error(2, "multGauss: Bad output pixel type\n"));
-  if ((my LT 0.0) OR (my GT 255.0))
-    return(Error(3, "multGauss: Bad my value\n"));
-  if (sigma LT 0.0)
-    return(Error(4, "multGauss: Bad sigma value\n"));
-  xsize = MIN(Ixsize(b1),Ixsize(b2));
-  ysize = MIN(Iysize(b1),Iysize(b2));
-
-  FOR (y=1; y LE ysize; y++)
-    FOR (x=1; x LE xsize; x++)
-      tmp = (int)((double)b1[y][x] * gaussRandom(my, sigma) + 0.5);
-      if (tmp LT 0) b2[y][x] = 0;
-      else if (tmp GT 255) b2[y][x] = 255;
-      else b2[y][x] = (UNS_BYTE)tmp;
-    ENDFOR;
-  ENDFOR;
-  return(0);
-} /* int multGauss */
-
-#endif /* not MAIN */
-
-
-
 /*P:multGauss*
 
 ________________________________________________________________
@@ -141,8 +71,6 @@ ________________________________________________________________
 
 */
 
-#ifdef MAIN
-
 int main(int argc, char **argv)
 {
   IMAGE i1, i2;
@@ -172,5 +100,3 @@ int main(int argc, char **argv)
   Iwrite_image(i2, argv[2]);
    return(0);
 }
-
-#endif /* MAIN */
