c045447d35a7ceda021a22dd0769c1befef8869f
eran <>
2008-12-17 10:12:36 +0000

    Renamed resample.c to resample_main.c.

--- src/src/resample/resample_main.c
+++ src/src/resample/resample_main.c
@@ -40,67 +40,6 @@ static char *Id = "$Id$, Blab, UiO";
 #include <xite/blab.h>
 #include <xite/message.h>
 
-#ifndef MAIN
-
-/*F:resample*
-
-________________________________________________________________
-
-		resample
-________________________________________________________________
-
-Name:		resample - stretch or compress a band
-
-Syntax:         | #include <xite/resample.h>
-		|
-                | int resample( IBAND b1, IBAND b2 );
-Description:    The data in input band b1 is resampled into output
-                band b2, using nearest neighbour resampling. Accepts
-		only unsigned byte pixels.
-
-Return value:   | 0 => ok
-                | 1 => Bad input pixel type
-                | 2 => Bad output pixel type
-
-Author:		Tor L|nnestad, BLAB, ifi, UiO
-
-Id:             $Id$
-________________________________________________________________
-
-*/
-
-int resample(IBAND b1, IBAND b2)
-{
-  int x, y, x1, y1, xsize2, ysize2;
-  float xfactor, yfactor, xoffset, yoffset;
-
-  if (Ipixtyp(b1) NE Iu_byte_typ)
-    return(Error(1, "resample: Bad input pixel type\n"));
-  if (Ipixtyp(b2) NE Iu_byte_typ)
-    return(Error(2, "resample: Bad output pixel type\n"));
-
-
-  xfactor = ((float)Ixsize(b1))/(float)Ixsize(b2);
-  yfactor = ((float)Iysize(b1))/(float)Iysize(b2);
-  xoffset = 1.0 - xfactor/2;
-  yoffset = 1.0 - yfactor/2;
-  xsize2 = Ixsize(b2);
-  ysize2 = Iysize(b2);
-
-  FOR (y=1; y LE ysize2; y++)
-    FOR (x=1; x LE xsize2; x++)
-      x1 = (int)(xoffset + xfactor*x);
-      y1 = (int)(yoffset + yfactor*y);
-      b2[y][x] = b1[y1][x1];
-    ENDFOR;
-  ENDFOR;
-  return 0;
-}
-
-#endif /* not MAIN */
-
-
-
 /*P:resample*
 
 ________________________________________________________________
@@ -134,8 +73,6 @@ ________________________________________________________________
 
 */
 
-#ifdef MAIN
-
 int main(int argc, char **argv)
 {
   IMAGE i1, i2;
@@ -165,5 +102,3 @@ int main(int argc, char **argv)
   Iwrite_image(i2, argv[2]);
    return(0);
 }
-
-#endif /* MAIN */
