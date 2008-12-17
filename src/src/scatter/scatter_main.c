f6c40d2d4457a330747728758771ee4ed00b6a10
eran <>
2008-12-17 10:16:07 +0000

    Renamed scatter.c to scatter_main.c.

--- src/src/scatter/scatter_main.c
+++ src/src/scatter/scatter_main.c
@@ -41,95 +41,6 @@ static char *Id = "$Id$, Blab, UiO";
 #include <xite/message.h>
 #include <xite/readarg.h>
 
-#ifndef MAIN
-
-#ifndef MIN
-# define MIN(a,b) (((a)<(b)) ? (a) : (b))
-#endif
-
-
-
-/*F:scatter*
-
-________________________________________________________________
-
-		scatter
-________________________________________________________________
-
-Name:		scatter - make a scatterplot
-
-Syntax:         | #include <xite/scatter.h>
-		|
-                | int scatter( IBAND input1, IBAND input2,
-                |    IBAND output, int *overflows );
-		|
-Description:	Creates a scatterplot (a two dimensional
-                histogram) of the two input bands. Every
-		scatter update that would increase the
-		scatter value beyond 255 increases the
-		parameter 'overflows' instead.
-
-Restrictions:   All bands must have pixel type unsigned byte.
-
-Return value:	| 0 => ok
-                | 1 => Bad pixel type in input1
-                | 2 => Bad pixel type in input2
-                | 3 => Bad pixel type in output
-		| 4 => Output has xsize less than 256
-		| 5 => Output has ysize less than 256
-
-Author:		Tor Lønnestad, BLAB, ifi, UiO
-
-Id:             $Id$
-________________________________________________________________
-
-*/
-
-
-
-int scatter(IBAND input1, IBAND input2, IBAND output, int *overflows)
-{
-   int x, y, val1, val2, xsize, ysize;
-
-   *overflows = 0;
-   if (Ipixtyp(input1) NE Iu_byte_typ)
-     return(Error(1, "%s\n",
-		  "scatter(): input1 must have pixel type unsigned byte."));
-   if (Ipixtyp(input2) NE Iu_byte_typ)
-     return(Error(2, "%s\n",
-		  "scatter(): input2 must have pixel type unsigned byte."));
-   if (Ipixtyp(output) NE Iu_byte_typ)
-     return(Error(3, "%s\n",
-		  "scatter(): output must have pixel type unsigned byte."));
-   if (Ixsize(output) LT 256)
-     return(Error(4, "scatter(): Output has xsize less than 256.\n"));
-   if (Iysize(output) LT 256)
-     return(Error(5, "scatter(): Output has ysize less than 256.\n"));
-
-   xsize = MIN(Ixsize(input1),Ixsize(input2));
-   ysize = MIN(Iysize(input1),Iysize(input2));
-
-   for (y=1; y LE 256; y++)
-     for (x=1; x LE 256; x++)
-       output[y][x] = 0;
-
-   FOR (y=1; y LE ysize; y++)
-     FOR (x=1; x LE xsize; x++)
-       val1 = input1[y][x]+1;
-       val2 = input2[y][x]+1;
-       if (output[val1][val2] LT 255)
-         ++ output[val1][val2];
-       else ++ (*overflows);
-     ENDFOR;
-   ENDFOR;
-   return 0;
-
-}  /*  scatter() */
-
-#endif /* not MAIN */
-
-
-
 /*P:scatter*
 
 ________________________________________________________________
@@ -162,8 +73,6 @@ Id:             $Id$
 ________________________________________________________________
 
 */
-
-#ifdef MAIN
 
 int main(int argc, char **argv)
 {
@@ -199,5 +108,3 @@ int main(int argc, char **argv)
    return(0);
 
 } /* main */
-
-#endif /* MAIN */
