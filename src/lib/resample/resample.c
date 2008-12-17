b8f7d152713b6a24354490603400c32ad9aa25b8
eran <>
2008-12-17 10:13:35 +0000

    Updated.

--- src/lib/resample/resample.c
+++ src/lib/resample/resample.c
@@ -40,8 +40,6 @@ static char *Id = "$Id$, Blab, UiO";
 #include <xite/blab.h>
 #include <xite/message.h>
 
-#ifndef MAIN
-
 /*F:resample*
 
 ________________________________________________________________
@@ -96,74 +94,3 @@ int resample(IBAND b1, IBAND b2)
   ENDFOR;
   return 0;
 }
-
-#endif /* not MAIN */
-
-
-
-/*P:resample*
-
-________________________________________________________________
-
-		resample
-________________________________________________________________
-
-Name:		resample - stretch or compress an image
-
-Syntax:		resample <inimage> <outimage> [<newxsize> <newysize> ...]
-
-Description:    Every band in the old image is stretched or compressed
-		to the size specified in the command line. You may
-		specify independant sizes for every band. If there are
-		more bands than size specifications, the last size
-		specification is applied to the rest of the bands.
-		Default sizes are 512x512.
-
-		The program uses the value found in the nearest pixel
-		in the old image, no interpolation is performed.
-
-Author:		Tor Lønnestad, BLAB, ifi, UiO
-
-Examples:       | resample mona.img largemona.img
-		| resample mona.img smallmona.img 256 256
-		| resample mona.img:1:1:1 monas.img 128 256 \\
-		|     256 128  256 256
-
-Id:             $Id$
-________________________________________________________________
-
-*/
-
-#ifdef MAIN
-
-int main(int argc, char **argv)
-{
-  IMAGE i1, i2;
-  int bn, xsize, ysize;
-
-  InitMessage(&argc, argv, xite_app_std_usage_text(
-     "Usage: %s <infile> <outfile> [<xsize> <ysize>...]\n"));
-  Iset_message(TRUE);
-  Iset_abort(TRUE);
-  if (argc == 1) Usage(1, (char*)0);
-  if (argc < 3) Usage(2, "Bad number of arguments\n");
-
-  i1 = Iread_image(argv[1]);
-  i2 = Init_image(Inbands(i1), Ititle(i1));
-  Icopy_text(i1, i2);
-  xsize = 512; ysize = 512;
-  FOR (bn=1; bn LE Inbands(i1); bn++)
-    if (argc GT 2*bn+1) xsize = atoi(argv[2*bn+1]);
-    if (argc GT 2*bn+2) ysize = atoi(argv[2*bn+2]);
-    if ((xsize LT 1) OR (ysize LT 1))
-      exit(Error(3, "Bad size specification\n"));
-    i2[bn] = Imake_band(Iu_byte_typ, xsize, ysize);
-    resample(i1[bn], i2[bn]);
-  ENDFOR;
-
-  Ihistory(i2, argv[0], "");
-  Iwrite_image(i2, argv[2]);
-   return(0);
-}
-
-#endif /* MAIN */
