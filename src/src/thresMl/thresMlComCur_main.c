1618f14140ddb53967a118977794428b8fbd2bb0
eran <>
2008-12-19 09:12:30 +0000

    Renamed thresMlComCur.c to thresMlComCur_main.c.

--- src/src/thresMl/thresMlComCur_main.c
+++ src/src/thresMl/thresMlComCur_main.c
@@ -40,159 +40,6 @@ static char *Id = "$Id$, Blab, UiO";
 
 
 
-#ifndef MAIN
-
-/*F:mkComCur*
-
-________________________________________________________________
-
-		mkComCur
-________________________________________________________________
-
-Name:		mkComCur - make complexity curve for a band
-
-Syntax:         | #include <xite/thresMl.h>
-		|
-                | int mkComCur( IBAND band, histogram cc );
-
-Description:    'mkComCur' produces the complexity curve of an N-bit
-                 image 'band' by calculating the lengths of black and white 
-                 boundaries for every possible threshold.
-
-		'cc' is the returned complexity curve, indexable on 0..255.
-
-Restrictions:   'band' must have pixel type unsigned byte.
-
-Return value:   | 0 => ok
-                | 2 => Bad pixel type
-
-Reference:      &Sei-ichiro Kamata, Richard O. Eason and Eiji Kawaguchi
-                "Complexity Curves Versus Histograms and Their Application
-                to Image Segmentation",	1070-1077 
-
-See also:	thresMl(1), thresMlAppScale(3), thresMlAppMean(3),
-                thresMlAppMedian(3), thresMlApply(3), histoCentroid(3),
-                thresMlCentroid(3), thresMlCentroid(3), thresMlComCur(3),
-                thresMlComCur(1), thresMlReddi(3), thresMlReddi(1),
-                thresMlWaHa(3), thresMlWaHa(1)
-
-Author:		Hung Buu Huynh, BLAB, Ifi, UiO
-
-Id: 		$Id$
-________________________________________________________________
-
-*/
-
-int mkComCur(IBAND band, int *cc)
-{
-  int B[257];
-  int n, x, y, n1, n2, xsize, ysize;
-
-  if (Ipixtyp(band) != Iu_byte_typ)
-    return(Error(2, "mkComCur: Input pixel type must be unsigned byte.\n"));
-
-  for (n=0; n<257; n++) B[n] = 0;
-  xsize = Ixsize(band);
-  ysize = Iysize(band);
-
-  for (y=1; y<=ysize; y++) {
-    for (x=1; x<=xsize-1; x++) {
-      if (band[y][x] < band[y][x+1])
-        { n1 = band[y][x] + 1; n2 = band[y][x+1] + 1; }
-      else
-        { n1 = band[y][x+1] + 1; n2 = band[y][x] + 1; }
-      B[n1]++; B[n2]--;
-    }
-  }
-
-  for (y=1; y<=ysize-1; y++) {
-    for (x=1; x<=xsize; x++) {
-      if (band[y][x] < band[y+1][x])
-        { n1 = band[y][x] + 1; n2 = band[y+1][x] + 1; }
-      else
-        { n1 = band[y+1][x] + 1; n2 = band[y][x] + 1; }
-      B[n1]++ ; B[n2]--  ;
-    }
-  }
-
-  cc[0] = B[0] ;   
-  for (n=1; n<256 ; n++) 
-     cc[n] = cc[n-1] + B[n];
-
-  return(0);
-
-} /* end of mkComCur */
-
-
-
-
-/*F:thresMlComCur*
-
-________________________________________________________________
-
-		thresMlComCur
-________________________________________________________________
-
-Name:		thresMlComCur - multi-level threshold using complexity curve
-
-Syntax:         | #include <xite/thresMl.h>
-		|
-                | int thresMlComCur( IBAND band, int* thresholds,
-                |    int nt );
-
-Description:    'thresMlComCur' performs multi-level thresholding of
-                the input 'band'. The result is a vector of 'thresholds',
-		with useful values in 0..'nt'-1. The algorithm is based
-		on Complexity Curves, see mkComCur(3). The complexity
-		curve is created, and then thresholded by means of the
-		algorithm or Reddi, see thresMlReddi(3).
-
-		'nt' is the desired number of thresholds.
-
-Restrictions:   'band' must have pixel type unsigned byte.
-
-Return value:   | 0 => ok
-                | 2 => Bad pixel type
-
-See also:	thresMl(1), thresMlAppScale(3), thresMlAppMean(3),
-                thresMlAppMedian(3), thresMlApply(3), histoCentroid(3),
-                thresMlCentroid(3), thresMlCentroid(3), mkComCur(3),
-                thresMlComCur(1), thresMlReddi(3), thresMlReddi(1),
-                thresMlWaHa(3), thresMlWaHa(1)
-
-Reference:      &Sei-ichiro Kamata, Richard O. Eason and Eiji Kawaguchi
-                "Complexity Curves Versus Histograms and Their Application
-                to Image Segmentation",	1070-1077
-
-Author:		Hung Buu Huynh, BLAB, Ifi, UiO
-
-Id: 		$Id$
-________________________________________________________________
-
-*/
-
-
-int thresMlComCur(IBAND band, int *thresholds, int nt)
-{
-  histogram cc;
-  int stat;
-
-  if (Ipixtyp(band) != Iu_byte_typ)
-    return(Error(2,
-		 "thresMlComCur: Input pixel type must be unsigned byte.\n"));
-
-  stat = mkComCur(band, cc);
-  if (stat) Warning(2, "Error in mkComCur.\n");
-  stat = thresMlReddi(cc, thresholds, nt);
-  if (stat) Warning(2, "Error in thresMlReddi.\n");
-
-  return(0);
-
-} /* end of thresMlComCur */
-
-
-#endif /* not MAIN */
-
 /*P:thresMlComCur*
 
 ________________________________________________________________
@@ -256,8 +103,6 @@ ________________________________________________________________
 
 */
 
-#ifdef MAIN
-
 int main(int argc, char **argv)
 {
    IMAGE img;
@@ -315,5 +160,3 @@ int main(int argc, char **argv)
 
    return(0);
 }
-
-#endif /* MAIN */
