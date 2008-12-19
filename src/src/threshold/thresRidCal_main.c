164b2275b7da45616bcf25f8ef35f55faf88cf5c
eran <>
2008-12-19 07:51:19 +0000

    Renamed thresRidCal.c to thresRidCal_main.c.

--- src/src/threshold/thresRidCal_main.c
+++ src/src/threshold/thresRidCal_main.c
@@ -42,81 +42,6 @@ static char *Id = "$Id$, Blab, UiO";
 
 
 
-#ifndef MAIN
-
-/*F:thresRidCal*
-
-________________________________________________________________
-
-		thresRidCal
-________________________________________________________________
-
-Name:		thresRidCal - threshold by the Ridler and Calvard method
-
-Syntax:         | #include <xite/threshold.h>
-		|
-                | int thresRidCal( histogram h );
-Description:	'thresRidCal' calculates the threshold value by using 
-		the method of Ridler and Calvard, 1972. Initially, 
-		the threshold value is set to the average brightness.
-		Then, iteratively, the threshold is set to the average
-		of the mean of the pixels below the threshold, and the
-		mean of the pixels above the threshold. When the
-		threshold value is constant, iteration terminates.
-
-		The actual thresholding is not performed, see the
-		routine "threshold".
-		
-Restrictions:    
-
-Return value:	threshold value. Pixel values up to, and including,
-		the returned threshold value, is below the threshold.
-		If any kind of error, -1 is returned.
-
-Author:		Tor L|nnestad, BLAB, ifi, UiO
-
-Id:             $Id$
-________________________________________________________________
-
-*/
-
-int thresRidCal(int *h)
-{
-   int t, i, newt, sumh, sumhi;
-   double meanbelow, meanabove;
-
-   sumh = 0; sumhi = 0;
-   FOR (i=0; i LE 255; INC i)
-     sumh += h[i];
-     sumhi += h[i]*i;
-   ENDFOR;
-   if (NOT sumh)
-     return(Error(-1, "thresRidCal: No pixels?\n"));
-   t = (int)((float)sumhi/(float)sumh + 0.5);
-
-   LOOP
-     sumh = 0; sumhi = 0;
-     FOR (i=0; i LE t; INC i)
-       sumh += h[i];
-       sumhi += h[i]*i;
-     ENDFOR;
-     meanbelow = (sumh) ? (double)sumhi/(double)sumh : (double)t;
-     sumh = 0; sumhi = 0;
-     FOR (i=t+1; i LE 255; INC i)
-       sumh += h[i];
-       sumhi += h[i]*i;
-     ENDFOR;
-     meanabove = (sumh) ? (double)sumhi/(double)sumh : (double)t;
-     newt = (int)((meanbelow+meanabove)/2.0 + 0.5);
-     if (newt EQ t) return(t);
-     t = newt;
-   ENDLOOP
-} /*  thresRidCal  */
-
-#endif /* not MAIN */
-
-
-
 /*P:thresRidCal*
 
 ________________________________________________________________
@@ -159,8 +84,6 @@ ________________________________________________________________
 
 */
 
-#ifdef MAIN
-
 int main(int argc, char **argv)
 {
    IMAGE i;
@@ -198,5 +121,3 @@ int main(int argc, char **argv)
 
    return(0);
 }
-
-#endif /* MAIN */
