ab76a7ebb45e8d9ad7204094625ed4a50997cae4
eran <>
2008-12-19 07:52:59 +0000

    Renamed thresSigma.c to thresSigma_main.c.

--- src/src/threshold/thresSigma_main.c
+++ src/src/threshold/thresSigma_main.c
@@ -46,75 +46,6 @@ static char *Id = "$Id$, Blab, UiO";
 
 
 
-#ifndef MAIN
-
-/*F:thresSigma*
-
-________________________________________________________________
-
-		thresSigma
-________________________________________________________________
-
-Name:		thresSigma - threshold at fraction of standard deviation
-
-Syntax:         | #include <xite/threshold.h>
-		|
-		| int thresxsigma(h, x)  
-                | histogram h;
-		| double x;
-
-                | int thresSigma( histogram h, double x );
-                |
-Description:	'thresSigma' calculates the threshold value by finding
-                the mean (mu) and the standard deviation (sigma) 
-		of the image, and then finding the threshold by 
-		the formula:
-
-		t = mu + (x * sigma)
-
-		The actual thresholding is not performed, see the
-		routine "threshold".
-		
-Return value:	The threshold value. Pixel values up to, and including,
-		the return value, is below the threshold.
-
-Author:		Olav Borgli, BLAB, ifi, UiO
-
-Id:             $Id$
-________________________________________________________________
-
-*/
-
-int thresSigma(int *h, double x)
-{ 
-  int t, i, size;
-  float mu, hn[256];
-  double sigma;
-
-  /* find size and norm. histo */
-  size = h[0];
-  for (i=1; i<=255; i++) size += h[i];
-  for (i=0; i<=255; i++) hn[i] = h[i]/(float)size;
-
-  /* find mean value */
-  mu = 0.0;
-  for (i=1; i<=255; i++) mu += hn[i]*i;
-
-  /* find standard deviation */
-  sigma = 0.0;
-  for (i=0; i<=255; i++) sigma += (i-mu)*(i-mu)*hn[i];
-  sigma = sqrt(sigma);
-  t = (int)(mu + x*sigma);
-
-  return(t);
-}
-
-#endif /* not MAIN */
-
-
-
-#ifdef MAIN
-
 /*P:thresSigma*
 
 ________________________________________________________________
@@ -199,5 +130,3 @@ int main(int argc, char **argv)
   
   return(0);
 }
-
-#endif /* MAIN */
