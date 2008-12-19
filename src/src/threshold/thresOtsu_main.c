626de17f7b43a7a4c464f7e775b4952f12dd208d
eran <>
2008-12-19 07:38:16 +0000

    Renamed thresOtsu.c to thresOtsu_main.c.

--- src/src/threshold/thresOtsu_main.c
+++ src/src/threshold/thresOtsu_main.c
@@ -43,80 +43,6 @@ static char *Id = "$Id$, Blab, UiO";
 
 
 
-#ifndef MAIN
-
-/*F:thresOtsu*
-
-________________________________________________________________
-
-		thresOtsu
-________________________________________________________________
-
-Name:		thresOtsu - threshold by the method of Otsu
-
-Syntax:         | #include <xite/threshold.h>
-		|
-                | int thresOtsu( histogram h );
-
-Description:	'thresOtsu' calculates the threshold value needed  
-                to threshold the image that the histogram 'h' is
-		created from, according to the method of Otsu.
-		The actual thresholding is not performed, see the
-		"threshold" routine.
-
-Reference:	&Otsu, N.
-                "A Threshold Selection Method from Gray-Level histograms"
-		IEEE Trans. Syst., Man and Cyb.,
-		Vol SMC-9, No.1, pp.62-66.		
-
-Return value:	The threshold value.
-
-See also:	thresOtsu(1), threshold(3), thresholdSpOut(3), thresPtile(3),
-                thresRidCal(3),	thresLloyd(3)
-
-Author:		Tor Lønnestad, BLAB, Ifi, UiO
-
-Id:             $Id$
-________________________________________________________________
-
-*/
-
-
-int thresOtsu(int *h)
-{
-   int i, bestsplitpos, cumh255;
-   double mean, splitval, bestsplitval, diff;
-   histogram cumh;
-   double cumhi[256];
-
-   cumh[0] = h[0]; cumhi[0] = 0.0;
-   for (i=1; i LE 255; INC i) {
-     cumh[i] = cumh[i-1] + h[i];
-     cumhi[i] = cumhi[i-1] + i*h[i];
-   }
-   cumh255 = cumh[255];
-   mean = cumhi[255]/cumh255;
-
-   bestsplitval = 0.0;
-   for (i=0; i LE 255; INC i) {
-     /* calc. split value */
-     if ((cumh[i] NE 0) AND (cumh[i] NE cumh255)) {
-       diff = mean*cumh[i] - cumhi[i];
-       splitval = (diff/((double)cumh[i]*(cumh255-cumh[i])))*diff;
-       if (splitval GT bestsplitval) {
-         bestsplitval = splitval; bestsplitpos = i;
-       }
-     }
-   }
-
-   return(bestsplitpos);
-
-}  /*  thresOtsu  */
-
-#endif /* not MAIN */
-
-
-
 /*P:thresOtsu*
 
 ________________________________________________________________
@@ -165,8 +91,6 @@ ________________________________________________________________
 
 */
 
-#ifdef MAIN
-
 int main(int argc, char **argv)
 {
    IMAGE input, output;
@@ -203,5 +127,3 @@ int main(int argc, char **argv)
 
    return(0);
 }
-
-#endif /* MAIN */
