0fd40c07af488a7693b15d4fa293634411b9ce65
eran <>
2008-12-23 11:09:20 +0000

    Copied BinM.h here from /hus/home/eran/src/Xite/2008-10-12/xite/lib/binarize.

---
+++ src/src/binarize/BinM.h
@@ -0,0 +1,38 @@
+
+
+/*
+
+________________________________________________________________
+
+        BinM.h
+        $Id$
+        Copyright 1990, Blab, UiO
+        Image processing lab, Department of Informatics
+        University of Oslo
+        E-mail: blab@ifi.uio.no
+________________________________________________________________
+  
+  Permission to use, copy, modify and distribute this software and its
+  documentation for any purpose and without fee is hereby granted, 
+  provided that this copyright notice appear in all copies and that 
+  both that copyright notice and this permission notice appear in supporting
+  documentation and that the name of B-lab, Department of Informatics or
+  University of Oslo not be used in advertising or publicity pertaining 
+  to distribution of the software without specific, written prior permission.
+
+  B-LAB DISCLAIMS ALL WARRANTIES WITH REGARD TO THIS SOFTWARE, INCLUDING ALL
+  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS, IN NO EVENT SHALL B-LAB
+  BE LIABLE FOR ANY SPECIAL, INDIRECT OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+  WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION
+  OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN 
+  CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ 
+
+*/
+
+
+#define MAX(a,b) ((a)>(b) ? a : b)
+#define MIN(a,b) ((a)<(b) ? a : b)
+
+#define SSIGN(a) (((a)<0) ? -1 : 1)
+#define SIGN(a) (((a)==0) ? 0 : (SSIGN(a)))
